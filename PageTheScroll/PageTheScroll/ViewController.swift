331342c9b85b56037742ff9a9cb9b5239bc0a176
Stefano Pernat <stefano.pernat@gmail.com>
2017-01-15 18:11:26 +0100

    PageTheScroll - Added images to the scrollView

--- PageTheScroll/PageTheScroll/ViewController.swift
+++ PageTheScroll/PageTheScroll/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for num in 0...2 {
+            let image = UIImage(named: "icon\(num).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            // set imageview position inside the scrollview
+            let imageX: CGFloat = view.frame.midX + view.frame.size.width * CGFloat(num)
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: imageX - 75, y: (view.frame.height / 2) - 75, width: 150, height: 150)
+        }
+        
+        print("Count: \(images.count)")
     }
 
     override func didReceiveMemoryWarning() {
